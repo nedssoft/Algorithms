f1a39f05c1c75cdb4420790c5ff9a46840002010
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-02-22 17:10:48 +0100

    Implement making change algorithm

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,8 +2,19 @@
 
 import sys
 
+denoms = [1, 5, 10, 25, 50]
 def making_change(amount, denominations):
-  pass 
+  
+  if (amount == 0):
+    return 1
+  elif amount < 0:
+    return 0
+  elif len(denominations) == 0 and amount >= 0:
+    return 0
+  else:
+    return making_change(amount - denominations[0], denominations) + making_change(amount, denominations[1:])
+
+making_change(0, denoms)
 
 
 if __name__ == "__main__":

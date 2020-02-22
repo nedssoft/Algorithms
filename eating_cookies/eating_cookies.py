135d036a9a023f56d3bf60bbe0186d1a19fbd90b
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-02-22 19:01:06 +0100

    Rock paper scissors implementation

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -9,7 +9,7 @@ import sys
 def eating_cookies(n, cache=None):
   if (n < 0):
     return 0
-  elif n < 1:
+  elif n == 0:
     return 1
   else:
     return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,18 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  plays = ['rock', 'paper', 'scissors']
+  outcomes = []
+  def inner_func(rounds, result =[]):
+    if rounds == 0:
+      outcomes.append(result)
+      return
+
+    for play in plays:
+      inner_func(rounds-1, result + [play])
+  inner_func(n, [])
+  return outcomes
+
 
 
 if __name__ == "__main__":
